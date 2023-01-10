4d285fa3fd6e8a30925eb996fd171c692677c699
jrk308 <johnny.b.khalife@sherwin.com>
2023-01-10 11:39:02 -0500

    Initial code commit

---
+++ types/my-switch/my-switch.stories.d.ts
@@ -0,0 +1,32 @@
+import "./index";
+declare const _default: {
+    title: string;
+    component: string;
+    argTypes: {
+        label: {
+            control: {
+                type: string;
+            };
+        };
+        labelPosition: {
+            control: {
+                type: string;
+            };
+            options: string[];
+        };
+        "--size": {
+            control: {
+                type: string;
+            };
+        };
+    };
+    parameters: {
+        actions: {
+            handles: string[];
+        };
+    };
+};
+export default _default;
+export declare const Default: any;
+export declare const ActionTest: any;
+export declare const IconTest: any;
